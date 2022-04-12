578684e684eec44c7a664d0e1c0813155ef70df3
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-04-12 03:35:07 +0100

    system call write instead of printf function which is not allowed

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 /**
@@ -10,13 +11,13 @@
  */
 int main(void)/*(int argc __attribute__((unused)), char **argv __attribute__((unused)))*/
 {
-	char *line;
+	char *line, *prompt = ">> ";
 	char **tokens;
 
 	for (;;)
 	{
 		/*handling the EOF signal here*/
-		printf(">> ");
+		write(STDOUT_FILENO, prompt, 3);
 		line = read_input();
 		tokens = split_input(line);
 		if (tokens[0] != NULL)
