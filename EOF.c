82001293be9dd4d97166a352e2a28095abc1d5a2
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-04-13 20:58:30 +0000

    end of file

--- EOF.c
+++ EOF.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include<stdio.h>
+/**
+ * end_offile - end of file
+ *
+ * Return: Nothing.
+ */
 
-int end_offile() {
-    int d;
-    while((d = getchar()) != EOF) {
-        printf("\"getchar()!=EOF\" result is %d\n", d);
-        printf("EOF:%d\n", EOF);
-    }
-        printf("\"getchar()!=EOF\" result is %d\n", d);
-        return(1);
+int end_offile(void)
+{
+int d;
+
+while ((d = getchar()) != EOF)
+{
+printf("\"getchar()!=EOF\" result is %d\n", d);
+printf("EOF:%d\n", EOF);
+}
+printf("\"getchar()!=EOF\" result is %d\n", d);
+return (1);
 }
